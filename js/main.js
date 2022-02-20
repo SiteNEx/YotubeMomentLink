49732edf30b6553d828eb1321ccfd4db79e7d0f1
Vladislav <stevl95@yandex.ru>
2022-02-20 21:15:25 +0400

    #2 Can add decomposed time to source link

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
 document.getElementById("cleanLink").onblur=printedCleanLink;
+document.getElementById("timeStr").onblur=printedTime;
 
 function printedCleanLink(){
-    let cleanLinkVal = document.getElementById("cleanLink").value;
-    // alert(cleanLinkVal);
+    const cleanLinkVal = document.getElementById("cleanLink").value;    
     if(cleanLinkVal==""){
         document.getElementById("resLink").innerHTML="Need link";
     }else{
         document.getElementById("resLink").innerHTML=cleanLinkVal;
-    }    
+    }
 }
+
+function printedTime(){
+    const timeStr = document.getElementById("timeStr").value;
+    const timeForLink=timeParts(timeStr);    
+    const cleanLinkVal = document.getElementById("cleanLink").value; 
+    if(timeForLink!=""){
+        document.getElementById("resLink").innerHTML=cleanLinkVal+"&t="+timeForLink;
+    }
+}
+
+function timeParts(timeStr) {
+    let resTimeStr="";
+    const parts=timeStr.trim().split(':');
+    if(parts.length==2){
+        //mm:ss
+        resTimeStr=parts[0]+"m"+parts[1]+"s";
+    }else  
+    if(parts.length==3){
+        //h:mm:ss
+        const h=parts[0];
+        const m=parts[1];
+        const s=parts[2];
+        resTimeStr=h+"h"+m+"m"+s+"s";
+    }  
+    return resTimeStr;  
+}
+
+
+
+
+
 
 
